783c696dc43d86bbef377721c6e380fb7211db2b
thom <thom@laptop.com>
2017-12-04 09:38:53 +0100

    monday morning, step 10

--- app/src/main/java/com/example/thom/restaurants2/OrderFragment.java
+++ app/src/main/java/com/example/thom/restaurants2/OrderFragment.java
@@ -6,16 +6,19 @@ import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.DialogFragment;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.ListView;
+import android.widget.TextView;
 
 
 /**
  * A simple {@link Fragment} subclass.
  */
-public class OrderFragment extends DialogFragment {
+public class OrderFragment extends DialogFragment implements View.OnClickListener  {
 
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
@@ -36,7 +39,25 @@ public class OrderFragment extends DialogFragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
 
+        View inflated = inflater.inflate(R.layout.fragment_order, container, false);
+        Button cancelButton = (Button) inflated.findViewById(R.id.buttonCancel);
+        cancelButton.setOnClickListener(this);
+
+        Button PlaceOrderButton = (Button) inflated.findViewById(R.id.buttonPlaceOrder);
+        PlaceOrderButton.setOnClickListener(this);
+
+
         return inflater.inflate(R.layout.fragment_order, container, false);
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch(view.getId()) {
+            case R.id.buttonPlaceOrder:
+                TextView textView = view.findViewById(R.id.textViewOrder);
+                textView.setText("order is placed");
+                Log.d("order status: ", "placed");
+        }
     }
 
     @Override
